8f06bf15cd7d7811b42f57c45570a22096222c7d
BeomJoon-MSB <ehdfkd@benter.co.kr>
2024-04-26 16:44:54 +0900

    Loader 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,11 @@
 import Logo from "../shared/components/Logo";
+import Loader from "../shared/components/Loader";
 import LoginForm from "./component/LoginForm";
 export default function Login() {
   return (
     <main>
       <div className="flex flex-col justify-center items-center">
+        <Loader />
         <Logo href="/" width={200} />
         <div className="w-3/5">
           <LoginForm />
