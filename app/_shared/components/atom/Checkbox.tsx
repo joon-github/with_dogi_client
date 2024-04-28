56e72a6df16e11f4caac32cc9e30d886baa403a4
joon-guthub <ehdfkd2000@naver.com>
2024-04-28 13:58:04 +0900

    약관동의

--- app/_shared/components/atom/Checkbox.tsx
+++ app/_shared/components/atom/Checkbox.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Checkbox } from "@nextui-org/react";
 
 export default function AtomCheckbox({ ...props }) {
-  return <Checkbox {...props} color="default" />;
+  return <Checkbox {...props} color="default" isRequired />;
 }
--- app/_shared/components/molecule/AgreementCheckbox.tsx
+++ app/_shared/components/molecule/AgreementCheckbox.tsx
@@ -1,14 +1,29 @@
 import { Checkbox } from "@/app/_shared/components/atom";
 
 interface Props {
+  value?: string;
   label: string | React.ReactNode;
   children?: React.ReactNode;
+  checked?: boolean;
+  onChange?: (isSelected: boolean) => void;
 }
-export default function AgreementCheckbox({ label, children }: Props) {
+export default function AgreementCheckbox({
+  value,
+  label,
+  children,
+  checked,
+  onChange,
+}: Props) {
   return (
     <div className="flex justify-between items-center">
       <div>
-        <Checkbox />
+        <Checkbox
+          value={value}
+          isSelected={checked}
+          onValueChange={(isSelected: boolean) => {
+            onChange(isSelected);
+          }}
+        />
         <span>{label}</span>
       </div>
       {children}
