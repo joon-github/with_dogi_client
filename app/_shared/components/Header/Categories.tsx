7ef769b6f8b56c8a400835c0a5abbc189423b767
joon-guthub <ehdfkd2000@naver.com>
2024-05-01 15:19:12 +0900

    카테고리 수정

--- app/_shared/components/Header/Categories.tsx
+++ app/_shared/components/Header/Categories.tsx
@@ -1,33 +1,6 @@
 "use client";
 import { useCategory } from "@/app/service/category/useCategoryService";
 import Link from "next/link";
-// export default function Categories() {
-//   const { data: categories } = useCategory("product");
-//   return (
-//     <ul className="flex flex-1 items-center gap-10 justify-around min-w-[1000px] border-y border-slate-400 py-2">
-//       {categories?.map((category) => (
-//         <li
-//           key={category.categoryId}
-//           className="flex flex-col items-center font-bold cursor-pointer group"
-//         >
-//           <Link href={`#`}>{category.categoryName}</Link>
-//           <div className="absolute mt-[30px] bg-white shadow-lg rounded-lg border p-2 hidden group-hover:block">
-//             {category.children &&
-//               category.children.length > 0 &&
-//               category.children.map((child) => (
-//                 <div
-//                   className="group relative min-w-[100px] w-fit py-2 text-center"
-//                   key={child.categoryId}
-//                 >
-//                   {child.categoryName}
-//                 </div>
-//               ))}
-//           </div>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
 
 export default function Categories() {
   const { data: categories } = useCategory("product");
@@ -43,12 +16,11 @@ export default function Categories() {
             {category.children &&
               category.children.length > 0 &&
               category.children.map((child) => (
-                <div
-                  className="min-w-[100px] w-fit py-2 text-center"
-                  key={child.categoryId}
-                >
-                  {child.categoryName}
-                </div>
+                <Link href={`#`} key={child.categoryId}>
+                  <div className="min-w-[100px] w-fit py-2 text-center">
+                    {child.categoryName}
+                  </div>
+                </Link>
               ))}
           </div>
         </li>
